771beff92d78d1d611d19aca06ddd0fbc639517d
adearya <ade.aryabimantara@gmail.com>
2024-11-02 21:24:08 +0700

    Learn props

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,20 @@ import Methods from './components/Methods.vue'
 import ComputedGetterSetter from './components/ComputedGetterSetter.vue'
 import WatchEffect from './components/WatchEffect.vue';
 import Watch from './components/Watch.vue';
+import Props from './components/Props.vue';
+
+import { ref, reactive } from "vue";
+
+const label = ref("Biodata");
+const user = reactive({
+  name: "Ade Arya Bimantara"
+});
+
+const changeName = () => {
+  user.name = "Joya Octaviana";
+};
 </script> 
  
 <template> 
-  <Watch /> 
+  <Props :label="label" :user="user" :changeName="changeName" /> 
 </template>
---
+++ src/components/Props.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+
+  user: {
+    type: Object,
+    default: () => ({ name: '' })
+  },
+
+  changeName: {
+    type: Function,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <p>{{ label }} dari {{ user.name }}</p>
+  <button @click="changeName">Change</button>
+</template>
